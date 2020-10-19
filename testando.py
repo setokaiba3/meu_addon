18990b7722375cf8a865cf893639c41e8ebfb048
setokaiba3 <73113191+setokaiba3@users.noreply.github.com>
2020-10-19 10:34:48 -0300

    Update testando.py

--- testando.py
+++ testando.py
@@ -1,1 +1,1 @@
-http://srvx.io:80/49119217125/snakeplay/71770
+https://store-images.s-microsoft.com/image/apps.54991.68307864442603829.676cbe70-1643-4bcc-a72b-f8ed4e2d4cc2.d5590cd0-239c-4d51-8bbd-363ae7761d52?mode=scale&q=90&h=1080&w=1920

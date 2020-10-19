83576c5ddae08d6083abd70134496dddfaf0dbf7
setokaiba3 <73113191+setokaiba3@users.noreply.github.com>
2020-10-19 11:09:23 -0300

    Update testando.py

--- testando.py
+++ testando.py
@@ -1,1 +1,1 @@
-http://s2.glbimg.com/7B_srQyXERP_NoJCHSzgOnuwsyc=/smart/e.glbimg.com/og/ed/f/original/2017/07/26/best-sapce-photos-2017-designboom-10.jpg
+https://www.tvovermind.com/wp-content/uploads/2018/11/Naruto-Shipp%C5%ABden.jpg

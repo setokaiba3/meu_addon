0fcc6d4ddc8b8776a362d431227db47252693616
setokaiba3 <73113191+setokaiba3@users.noreply.github.com>
2020-10-22 13:15:32 -0300

    Update redecanais.py

--- redecanais.py
+++ redecanais.py
@@ -1,23 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# Copyright 2014
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
+-*-
 ##############BIBLIOTECAS A IMPORTAR E DEFINICOES####################
 
-import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmc,xbmcaddon,string,base64,os,urlresolver
+import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmc,xbmcaddon,string,base64,os,urlresolver,sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 from BeautifulSoup import BeautifulSoup
 
 pluginhandle = int(sys.argv[1])
